992bcd80b6c9f564569623e3008a130d9cf60c75
Md. Mahmudul Hasan Shohag <imaginativeshohag@yahoo.com>
2016-07-19 04:04:11 +0600

    v1.1 ================= # Change Flashlight code and add support for:     - Under Marshmallow support     - After Marshmallow using new builtin api * Update/Extend Color Picker

--- app/src/main/java/org/imaginativeworld/fan/MainActivity.java
+++ app/src/main/java/org/imaginativeworld/fan/MainActivity.java
@@ -4,37 +4,56 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+/**
+ * Here Flashlight codes are brought from:
+ * https://github.com/jbutewicz/Flashlight-by-Joe
+ */
+
 package org.imaginativeworld.fan;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.camera2.CameraManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements PreviewSurface.Callback, View.OnClickListener, View.OnTouchListener {
+import java.io.IOException;
+import java.util.List;
 
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SurfaceHolder.Callback, View.OnTouchListener {
+    //PreviewSurface.Callback,
     final int TOTAL_FAN_MODEL = 7;
 
     ImageView imgFan;
@@ -49,9 +68,17 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
     /**
      * Flash Light variables/objects
      */
-    PreviewSurface mSurface;
     boolean on = false;
-    boolean mCameraReady = false; // to make sure we don't turn on light when preview surface resizes
+    //boolean mCameraReady = false; // to make sure we don't turn on light when preview surface resizes
+    String mCameraId;
+    //PreviewSurface mSurface;
+    boolean torchRequest = false;
+
+    int count = 0;
+    static Camera mCameraActivity;
+    Parameters params;
+    SurfaceView preview;
+    SurfaceHolder mHolder;
 
     MediaPlayer mp;
 
@@ -64,6 +91,11 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
     String _pref_fan_color;
     Boolean _pref_fan_sound;
     int _pref_fan_model;
+
+    /**
+     * The View
+     */
+    ViewGroup rootView;
 
 
     @Override
@@ -93,11 +125,29 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
 
         fabLight = (FloatingActionButton) findViewById(R.id.fabLight);
 
+        rootView = (ViewGroup) ((ViewGroup) this
+                .findViewById(android.R.id.content)).getChildAt(0);
+
         /**
          * Needed For camera
          */
-        mSurface = (PreviewSurface) findViewById(R.id.surfaceView);
-        mSurface.setCallback(this);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            RelativeLayout context_main_layout = (RelativeLayout) findViewById(R.id.context_main_layout);
+
+            RelativeLayout.LayoutParams lpView = new RelativeLayout.LayoutParams(dpToPx(1), dpToPx(1));
+            preview = new SurfaceView(MainActivity.this);
+            preview.setLayoutParams(lpView);
+            context_main_layout.addView(preview);
+
+            try {
+                setmCameraActivity(Camera.open());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // Last
+            //mSurface.setCallback(MainActivity.this);
+        }
 
         /**
          * Now Load settings form preference ans set
@@ -179,6 +229,9 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
             }
         });
 
+        /**
+         * Fan Switch
+         */
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -188,11 +241,6 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
                     imgFan.startAnimation(anim);
 
                     try {
-//                        if (mp.isPlaying()) {
-//                            mp.stop();
-//                            mp.release();
-//                            mp = MediaPlayer.create(MainActivity.this, R.raw.sound02);
-//                        }
 
                         if (isSoundOn)
                             mp.start();
@@ -210,7 +258,6 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
                         if (mp.isPlaying()) {
                             mp.pause();
                         }
-//                        mp.start();
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -225,39 +272,270 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
         hasFlash = getApplicationContext().getPackageManager()
                 .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
+        /**
+         * Flashlight switch
+         */
         fabLight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!hasFlash) {
-                    Snackbar.make(view, "Flashlight not found! Please let us know if you think this is a Bug.", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                    turnOnOffTourchForMplus();
+
                 } else {
-                    if (!on) turnOn();
-                    else turnOff();
+
+                    if (torchRequest) {
+
+                        torchRequest = !torchRequest;
+                        processOffClick();
+                        setTorchIconStyle(false);
+
+                    } else {
+
+                        torchRequest = !torchRequest;
+                        processOnClick();
+                        setTorchIconStyle(true);
+
+                    }
+
                 }
 
             }
         });
 
+    }
 
+    public int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        return px;
+    }
+
+    void turnOnOffTourchForMplus() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            torchRequest = !torchRequest;
+
+            try {
+                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+//                for (String id : cameraManager.getCameraIdList()) {
+                mCameraId = cameraManager.getCameraIdList()[0];
+
+                // Turn on the flash if camera has one
+//                if (cameraManager.getCameraCharacteristics(mCameraId).get(CameraCharacteristics.FLASH_INFO_AVAILABLE)) {
+                cameraManager.setTorchMode(mCameraId, torchRequest);
+//                }
+
+                setTorchIconStyle(torchRequest);
+//                }
+            } catch (Exception e) {
+                Log.e(getString(R.string.TAG), "Failed to interact with camera.", e);
+                Toast.makeText(MainActivity.this, "Torch Failed: " + e.getMessage(), Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+
+    void setTorchIconStyle(boolean isOn) {
+        if (isOn) {
+            fabLight.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorAccent)));
+            fabLight.setColorFilter(getResources().getColor(android.R.color.white));
+        } else {
+            fabLight.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.white)));
+            fabLight.setColorFilter(Color.parseColor("#263238"));
+        }
+    }
+
+
+    /**
+     * Turns the LED on when the button on the app is pressed.
+     */
+    // Any time the widget or the button in the app is pressed to turn the LED
+    // on we process this off click. On method that is deprecated is needed for
+    // earlier than Android 3.0 devices.
+    //@SuppressWarnings("deprecation")
+    private void processOnClick() {
+
+        //flashlight_button.setBackgroundResource(R.drawable.light_off);
+        //setWidgetTo(R.drawable.light_off);
+
+        if (getmCameraActivity() == null) {
+            try {
+                mHolder.addCallback(this);
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+                    mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+                }
+                setmCameraActivity(Camera.open());
+                try {
+                    if (mHolder != null) {
+                        getmCameraActivity().setPreviewDisplay(mHolder);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (getmCameraActivity() != null) {
+            flashOnApp();
+        }
+
+        turnMotorolaOn();
+
+    }
+
+    private void flashOnApp() {
+
+        Log.d(getString(R.string.TAG), "Clicked");
+
+        setParams(getmCameraActivity().getParameters());
+
+        List<String> flashModes = getParams().getSupportedFlashModes();
+
+        if (flashModes == null) {
+            return;
+        } else {
+            if (count == 0) {
+                getParams().setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                getmCameraActivity().setParameters(getParams());
+                //preview = (SurfaceView) findViewById(R.id.preview);
+                mHolder = preview.getHolder();
+                mHolder.addCallback(MainActivity.this);
+
+                try {
+                    getmCameraActivity().startPreview();
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+
+            }
+
+            String flashMode = getParams().getFlashMode();
+
+            if (!Camera.Parameters.FLASH_MODE_TORCH.equals(flashMode)) {
+
+                if (flashModes.contains(Camera.Parameters.FLASH_MODE_TORCH)) {
+                    getParams().setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                    getmCameraActivity().setParameters(getParams());
+                    Log.d(getString(R.string.TAG), "Turned On");
+                } else {
+                    getParams().setFlashMode(Camera.Parameters.FLASH_MODE_ON);
+
+                    getmCameraActivity().setParameters(getParams());
+
+                    Log.d(getString(R.string.TAG), "Turned Off");
+
+                    try {
+                        getmCameraActivity().autoFocus(new Camera.AutoFocusCallback() {
+                            public void onAutoFocus(boolean success,
+                                                    Camera camera) {
+                                count = 1;
+                            }
+                        });
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+    }
+
+
+    static void turnMotorolaOn() {
+        DroidLED led;
+        try {
+            led = new DroidLED();
+            led.enable(true);
+            //AppGlobals.setIsFlashOn(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Any time the widget or the button in the app is pressed to turn the LED
+    // off we process this off click.
+    private void processOffClick() {
+
+        if (getmCameraActivity() != null) {
+            count = 0;
+            flashOffApp();
+        }
+
+        turnMotorolaOff();
+
+    }
+
+    // Turns the LED off when the button on the app is pressed.
+    private void flashOffApp() {
+        getmCameraActivity().stopPreview();
+        getmCameraActivity().release();
+        setmCameraActivity(null);
+    }
+
+
+    // Turns the LED off for some Motorola phones.
+    static void turnMotorolaOff() {
+        DroidLED led;
+        try {
+            led = new DroidLED();
+            led.enable(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // Getters and setters for mCameraActivity.
+    public static Camera getmCameraActivity() {
+        return mCameraActivity;
+    }
+
+    public static void setmCameraActivity(Camera mCameraActivity) {
+        MainActivity.mCameraActivity = mCameraActivity;
+    }
+
+    // Getters and setters for params.
+    public Parameters getParams() {
+        return params;
+    }
+
+    public void setParams(Parameters params) {
+        this.params = params;
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         /**
-         * Don't need to turnoff
+         * Don't need to turnoff camera
          */
 //        turnOff();
 //        mSurface.releaseCamera();
 //        paused = true;
+
+        /**
+         * Stop Fan Turned Off
+         */
+        imgFan.clearAnimation();
+
+        try {
+            if (mp.isPlaying()) {
+                mp.pause();
+            }
+        } catch (Exception e) {
+            // NOTE: didn't think need any exception here
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         /**
-         * Don't need to turnoff
+         * Don't need to turnoff camera
          */
 //        if (paused) {
 //            mSurface.initCamera();
@@ -372,22 +650,30 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
         int id = item.getItemId();
 
         if (id == R.id.action_change_fan_color) {
+
             Intent color_intent = new Intent(MainActivity.this, colorPicker.class);
+            color_intent.putExtra(getString(R.string.Color), strColor);
             startActivityForResult(color_intent, 100);
 
             return true;
+
         } else if (id == R.id.action_about) {
 
             Intent about_intent = new Intent(MainActivity.this, aboutActivity.class);
             startActivity(about_intent);
 
             return true;
+
         } else if (id == R.id.action_contact_us) {
+
             sendEmail();
+
         } else if (id == R.id.action_rate) {
+
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse(getResources().getString(R.string.url_market)));
             startActivity(intent);
+
         } else if (id == R.id.action_change_fan_model) {
 
             if (_pref_fan_model == TOTAL_FAN_MODEL) {
@@ -396,6 +682,7 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
                 _pref_fan_model++;
             }
             setFanModel(_pref_fan_model);
+
         }
 
 
@@ -409,6 +696,7 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
         switch (requestCode) {
             case 100: // color activity
                 if (resultCode == RESULT_OK) {
+
                     Bundle res = data.getExtras();
                     strColor = res.getString("color");
 
@@ -418,39 +706,6 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
                 break;
 
         }
-
-    }
-
-    private void turnOn() {
-        if (!on) {
-            on = true;
-            mSurface.lightOn();
-            fabLight.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorAccent)));
-            fabLight.setColorFilter(getResources().getColor(android.R.color.white));
-
-        }
-    }
-
-    private void turnOff() {
-        if (on) {
-            on = false;
-            mSurface.lightOff();
-            fabLight.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.white)));
-            fabLight.setColorFilter(Color.parseColor("#263238"));
-        }
-    }
-
-
-    @Override
-    public void cameraReady() {
-        if (!mCameraReady) {
-            mCameraReady = true;
-            //turnOn();
-        }
-    }
-
-    @Override
-    public void cameraNotAvailable() {
 
     }
 
@@ -539,5 +794,42 @@ public class MainActivity extends AppCompatActivity implements PreviewSurface.Ca
             }
         }
         return true;
+    }
+
+    // The following three methods are needed to implement SurfaceView.Callback.
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mHolder = holder;
+        mHolder.addCallback(this);
+
+        if (getmCameraActivity() != null) {
+
+            try {
+                getmCameraActivity().setPreviewDisplay(mHolder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+        mHolder = holder;
+        mHolder.addCallback(this);
+
+        if (getmCameraActivity() != null) {
+            try {
+                getmCameraActivity().setPreviewDisplay(mHolder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mHolder = null;
     }
 }
